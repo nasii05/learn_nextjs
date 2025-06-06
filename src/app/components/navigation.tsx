572cc7221ce6023fc4788a664dd321761742ff6e
nasii05 <iamnasih@gmail.com>
2025-06-06 13:52:20 +0530

    define(navigation paths)

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -8,13 +8,14 @@ export const Navigation = () => {
 
     return (
         <nav>
-            <Link href="/" className={pathname === "/" ? "font-bold mr-4" : "text-blue-500 mr-3"}>
+            <Link href="/" className={pathname === "/" ? "font-bold mr-4" : "text-blue-500 mr-4"}>
                 Home
             </Link>
-            <Link href="/about" className="mr-4 text-blue-500">
+            <Link href="/about" className={pathname === "/about" ? "font-bold mr-4" : "text-blue-500 mr-4"}>
                 About
             </Link>
-            <Link href="/products/1" className="mr-4 text-blue-500">
+            <Link href="/products/1" className={pathname.startsWith("/products/1") ?
+                "font-bold mr-4" : "text-blue-500 mr-4"}>
                 Product
             </Link>
         </nav>
