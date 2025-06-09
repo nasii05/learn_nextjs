34f31bcd09b78e6f111e165c0e677aa4c64afe26
nasii05 <iamnasih@gmail.com>
2025-06-09 14:30:13 +0530

    user input

--- src/app/mock-users/page.tsx
+++ src/app/mock-users/page.tsx
@@ -5,7 +5,7 @@ type MockUser = {
 
 export default async function MockUsers() {
   const res = await fetch("https://68468d087dbda7ee7aaf6c87.mockapi.io/users", {
-    cache: "no-store", // Optional: ensures fresh data
+    cache: "no-store",
   });
 
   if (!res.ok) {
@@ -15,15 +15,26 @@ export default async function MockUsers() {
   const users: MockUser[] = await res.json();
 
   return (
-    <div className="grid grid-cols-4 gap-4 py-10 px-4">
-      {users.map((user) => (
-        <div
-          key={user.id}
-          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+    <div className="py-10">
+      <form className="mb-4">
+        <input type="text" name="name" required className="border p-2 mr-4" />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded"
         >
-          {user.name}
-        </div>
-      ))}
+          Add User
+        </button>
+      </form>
+      <div className="grid grid-cols-4 gap-4 py-10 px-4">
+        {users.map((user) => (
+          <div
+            key={user.id}
+            className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+          >
+            {user.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
